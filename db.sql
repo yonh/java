813c5683c36094567718373d004129c0d97f653b
yonh <laiyongheng@yeseyao.com>
2022-01-15 17:40:06 +0800

    hello jdbc

---
+++ db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE hello CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+insert into user values(1,'admin');
